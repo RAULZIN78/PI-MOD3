21ac9d8a6546f092a28ab902e1040230e44b031a
RAULZIN78 <rlfs@etepd.com>
2025-02-24 11:09:19 -0300

    Add files via upload

---
+++ images/img.js
@@ -0,0 +1,10 @@
+import notebook from './notebook.png';
+import eteLogo from './eteLogo.png';
+import imageLogin from './imagemLogin.jpeg';
+import marcoZero from './marcoZero.png';
+import estudantes from './estudantes.png';
+import logoPL from './LogoPL.png';
+import user from './user.png';
+import menuHamburg from './menuHamburg.png';
+
+export { notebook, eteLogo, imageLogin, marcoZero, estudantes, logoPL, user, menuHamburg};
